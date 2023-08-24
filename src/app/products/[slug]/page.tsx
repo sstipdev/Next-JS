84ee5a1d4cb668e41b5a3af5f173f3b53697a2f1
lk1 <whitecandy777@naver.com>
2023-08-24 21:56:13 +0900

    페이지 미리 생성

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,3 +1,5 @@
+// 동적 라우팅 타입
+// 폴더명이 [slug] 가 아닌 [item] 이라면 params의 키 값은 slug가 아닌 item으로 해야한다.
 type Props = {
   params: {
     slug: string;
@@ -6,4 +8,14 @@ type Props = {
 
 export default function PantsPage({ params }: Props) {
   return <h1>{params.slug} 제품 설명 페이지</h1>;
+}
+
+// Next JS에서 약속한 함수
+// 특정한 경로 한에서 페이지를 만듬
+export function generateStaticParams() {
+  const products = ["pants", "skirt"];
+
+  return products.map((product) => ({
+    slug: product,
+  }));
 }
