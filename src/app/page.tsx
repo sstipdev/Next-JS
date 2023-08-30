a04f4e6835ec31e18ae96c5f6309b09cb8cf3953
lk1 <whitecandy777@naver.com>
2023-08-30 23:56:37 +0900

    server, client 컴포넌트 렌더링

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
 import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "./page.module.css";
+import os from "os"; // node api
+import Counter from "@/components/Counter";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <h1>홈페이지다!!</h1>;
+  console.log("hi");
+  console.log(os.hostname());
+  return (
+    <>
+      <h1>홈페이지다!!</h1>
+      <Counter />
+    </>
+  );
 }
