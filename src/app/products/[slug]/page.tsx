dc9b1bcacebe9479128f78b55f29828b9bfceb2c
lk1 <whitecandy777@naver.com>
2023-09-03 00:44:23 +0900

    리팩토링(설계)

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,5 +1,6 @@
 // 동적 라우팅 타입
 
+import { getProduct, getProducts } from "@/service/products";
 import { notFound } from "next/navigation";
 
 // 폴더명이 [slug] 가 아닌 [item] 이라면 params의 키 값은 slug가 아닌 item으로 해야한다.
@@ -15,19 +16,18 @@ export function generateMetadata({ params }: Props) {
   };
 }
 
-export default function PantsPage({ params }: Props) {
-  // 상품 라우터 내의 notFound 함수 호출
-  if (params.slug === "nothing") {
+export default function PantsPage({ params: { slug } }: Props) {
+  const product = getProduct(slug);
+  if (!product) {
     notFound();
   }
-  return <h1>{params.slug} 제품 설명 페이지</h1>;
+  return <h1>{product} 제품 설명 페이지</h1>;
 }
 
 // Next JS에서 약속한 함수
 // 특정한 경로 한에서 페이지를 만듬
 export function generateStaticParams() {
-  const products = ["pants", "skirt"];
-
+  const products = getProducts();
   return products.map((product) => ({
     slug: product,
   }));
