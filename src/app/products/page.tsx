9edbaaed7ec7b82688e9308ee258ae210f13fc08
lk1 <whitecandy777@naver.com>
2023-09-03 19:24:01 +0900

    fetch를 사용한 SSG, ISR, SSR

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,15 +1,22 @@
 import { getProducts } from "@/service/products";
 import Link from "next/link";
+import styles from "./page.module.css";
 
 /**
  * revalidate default값은 false이기에 ssg로 동작한다.
  * revalidate 0의 값은 ssr 처럼 동작하여 요청이 올때마다 만든다.
  * revalidate 3의 값은 3초 마다 revalidate 처리를 한다.
  */
-export const revalidate = 3;
+// export const revalidate = 3;
 
 export default async function ProductsPage() {
   const products = await getProducts();
+  const res = await fetch("https://meowfacts.herokuapp.com", {
+    // 3초 간격으로 fetch
+    next: { revalidate: 3 },
+  });
+  const data = await res.json();
+  const factText = data.data[0];
   return (
     <>
       <h1>제품 소개 페이지!</h1>
@@ -22,6 +29,7 @@ export default async function ProductsPage() {
           );
         })}
       </ul>
+      <article className={styles.article}>{factText}</article>
     </>
   );
 }
